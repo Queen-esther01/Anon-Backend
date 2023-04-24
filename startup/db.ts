91baf116ee888ab3f9f0c5734cbc361a3d621f73
Esther <ejidikeesther@gmail.com>
2023-04-24 02:41:43 +0100

    Add Mongo Prod DB

--- startup/db.ts
+++ startup/db.ts
@@ -3,9 +3,10 @@ const log = require('../utils/Logger')
 const mongoose = require('mongoose')
 
 
-
+//yZrrbpFCY7aQzNEt
 module.exports = function db(){
-    mongoose.connect('mongodb://127.0.0.1:27017/anon')
+    const env = process.env.NODE_ENV || "development"
+    mongoose.connect(env === 'development' ? process.env.DEV_MONGO_URL : process.env.PROD_MONGO_URL)
     .then(()=> log.info('Connected to anon database...'))
     .catch((err: unknown) => console.error('Could not connect to anon database...', err))
 
