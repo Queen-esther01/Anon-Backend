aa901f77bfbda507661813313d2673f09d2d7a47
Esther <ejidikeesther@gmail.com>
2023-04-24 03:02:42 +0100

    Trigger Build

--- startup/db.ts
+++ startup/db.ts
@@ -1,4 +1,3 @@
-import { Schema } from 'mongoose'
 const log = require('../utils/Logger')
 const mongoose = require('mongoose')
 
@@ -8,6 +7,4 @@ module.exports = function db(){
     mongoose.connect(env === 'development' ? process.env.DEV_MONGO_URL : process.env.PROD_MONGO_URL)
     .then(()=> log.info('Connected to anon database...'))
     .catch((err: unknown) => console.error('Could not connect to anon database...', err))
-
-    //const MyModel = mongoose.model('Test', new Schema({ name: String }));
 }
